95d73892d41daed9200d5e907184472b20ea7904
kevin mijares <kevmdm@gmail.com>
2020-12-15 11:50:51 -0400

    singlechildscrollview

--- lib/src/pages/device_page.dart
+++ lib/src/pages/device_page.dart
@@ -26,74 +26,149 @@ class _DevicePageState1 extends State<DevicePage1> {
         child: Scaffold(
             body: Container(
       color: colorBackGround,
-      child: Column(children: [
-        Container(
-            height: 10.0,
-            width: double.infinity,
+      child: SingleChildScrollView(
+              child: Column(children: [
+          Container(
+              height: 10.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: gradiente,
+              )),
+          SizedBox(height: 26.0),
+          Container(
+            height: 123,
+            width: 123,
             decoration: BoxDecoration(
-              gradient: gradiente,
-            )),
-        SizedBox(height: 26.0),
-        Container(
-          height: 123,
-          width: 123,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(100.00)),
-          child: selectIcon(98.0, colorMedico, globalType),
-        ),
-        SizedBox(
-          height: 8.0,
-        ),
-        Text(
-          title,
-          style: TextStyle(
-              color: colorVN, fontSize: 40.0, fontWeight: FontWeight.w400),
-        ),
-        SizedBox(
-          height: 10.0,
-        ),
-        Text('Wifi Network',
+                color: Colors.white, borderRadius: BorderRadius.circular(100.00)),
+            child: selectIcon(98.0, colorMedico, globalType),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            title,
             style: TextStyle(
-                color: colorVN, fontSize: 25.0, fontWeight: FontWeight.w300)),
-        SizedBox(
-          height: 10.0,
-        ),
-        Container(
-            height: 100,
-            width: 360,
-            child: Column(children: [
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text('Name',
-                    style: TextStyle(
-                        color: colorVN,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w300)),
-              ),
-              SizedBox(
-                height: 10.00,
-              ),
-              Container(
+                color: colorVN, fontSize: 40.0, fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text('Wifi Network',
+              style: TextStyle(
+                  color: colorVN, fontSize: 25.0, fontWeight: FontWeight.w300)),
+          SizedBox(
+            height: 10.0,
+          ),
+          
+          Container(
+              height: 100,
+              width: 360,
+              child: Column(children: [
+                Container(
                   alignment: Alignment.topLeft,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(08.00),
-                    border: Border.all(width: 1, color: Colors.black26),
-                  ),
-                  child: Container(
-                    height: 40,
-                    alignment: Alignment.center,
-                    margin: const EdgeInsets.only(left: 08.0, right: 08.0),
-                    child: TextField(
+                  child: Text('Name',
                       style: TextStyle(
-                          color: Colors.black,
+                          color: colorVN,
                           fontSize: 25.0,
-                          fontWeight: FontWeight.w300),
-                      decoration: null,
+                          fontWeight: FontWeight.w300)),
+                ),
+                SizedBox(
+                  height: 10.00,
+                ),
+                Container(
+                    alignment: Alignment.topLeft,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(08.00),
+                      border: Border.all(width: 1, color: Colors.black26),
                     ),
-                  )),
-            ])),
-      ]),
+                    child: Container(
+                      height: 40,
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(left: 08.0, right: 08.0),
+                      child: TextField(
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w300),
+                        decoration: null,
+                      ),
+                    )),
+              ])),
+          SizedBox(height: 10.00),
+          Container(
+              height: 100,
+              width: 360,
+              child: Column(children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Text('Password',
+                      style: TextStyle(
+                          color: colorVN,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w300)),
+                ),
+                SizedBox(
+                  height: 10.00,
+                ),
+                Container(
+                    alignment: Alignment.topLeft,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(08.00),
+                      border: Border.all(width: 1, color: Colors.black26),
+                    ),
+                    child: Container(
+                      height: 40,
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(left: 08.0, right: 08.0),
+                      child: TextField(
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w300),
+                        decoration: null,
+                      ),
+                    )),
+              ])),
+          SizedBox(height: 20.00),
+           Container(
+              height: 100,
+              width: 360,
+              child: Column(children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Text('Device Name',
+                      style: TextStyle(
+                          color: colorVN,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w300)),
+                ),
+                SizedBox(
+                  height: 10.00,
+                ),
+                Container(
+                    alignment: Alignment.topLeft,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(08.00),
+                      border: Border.all(width: 1, color: Colors.black26),
+                    ),
+                    child: Container(
+                      height: 40,
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(left: 08.0, right: 08.0),
+                      child: TextField(
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w300),
+                        decoration: null,
+                      ),
+                    )),
+              ])),
+        ]),
+      ),
       //child:Colum()
     )));
   }
