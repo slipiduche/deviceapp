4c055c69031330fb7279cd37da7de9552ee51b07
slipiduche <48814217+slipiduche@users.noreply.github.com>
2021-03-08 09:58:07 -0400

    portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,65 @@
 import 'package:deviceapp/src/pages/home_page.dart';
 import 'package:deviceapp/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wifi_iot/wifi_iot.dart';
+
+/// Forces portrait-only mode application-wide
+/// Use this Mixin on the main app widget i.e. app.Dart
+/// Flutter's 'App' has to extend Stateless widget.
+///
+/// Call `super.build(context)` in the main build() method
+/// to enable portrait only mode
+mixin PortraitModeMixin on StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    _portraitModeOnly();
+    return null;
+  }
+}
+
+/// Forces portrait-only mode on a specific screen
+/// Use this Mixin in the specific screen you want to
+/// block to portrait only mode.
+///
+/// Call `super.build(context)` in the State's build() method
+/// and `super.dispose();` in the State's dispose() method
+mixin PortraitStatefulModeMixin<T extends StatefulWidget> on State<T> {
+  @override
+  Widget build(BuildContext context) {
+    _portraitModeOnly();
+    return null;
+  }
+
+  @override
+  void dispose() {
+    _enableRotation();
+  }
+}
+
+/// blocks rotation; sets orientation to: portrait
+void _portraitModeOnly() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+}
+
+void _enableRotation() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+}
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget with PortraitModeMixin {
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Roboto'),
       debugShowCheckedModeBanner: false,
